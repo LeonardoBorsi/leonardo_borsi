1b139b369fdd201d9f0a5ab80f9f668148baf020
Leonardo Borsi <leonardo.borsi@gmail.com>
2024-09-02 19:20:39 +0200

    removed footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,34 @@
-import "@/styles/globals.css";
-import { Link } from "@nextui-org/link";
-import { Metadata, Viewport } from "next";
+import '@/styles/globals.css'
+import { Metadata, Viewport } from 'next'
 
-import { Navbar } from "@/components/navbar";
-import { Providers } from "./providers";
+import { Navbar } from '@/components/navbar'
+import { Providers } from './providers'
 
 export const metadata: Metadata = {
-  title: "Leonardo Borsi",
+  title: 'Leonardo Borsi',
   description: "Leonardo Borsi's Portfolio Website",
   icons: {
-    icon: "favicon.ico",
-  },
-};
+    icon: 'favicon.ico'
+  }
+}
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' }
+  ]
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang='en'>
       <head />
-      <body className="min-h-screen bg-background font-sans antialiased ">
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-          <div className="relative flex flex-col h-auto">
+      <body className='min-h-screen bg-background font-sans antialiased '>
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'light' }}>
+          <div className='relative flex flex-col h-auto'>
             <Navbar />
-            <main className="container mx-auto max-w-screen-2xl px-5 lg:px-20 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
+            <main className='container mx-auto max-w-screen-2xl px-5 lg:px-20 flex-grow'>{children}</main>
+            {/*  <footer className="w-full flex items-center justify-center py-3">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
@@ -45,10 +38,10 @@ export default function RootLayout({
                 <span className="text-default-600">Powered by</span>
                 <p className="text-primary">NextUI</p>
               </Link>
-            </footer>
+            </footer> */}
           </div>
         </Providers>
       </body>
     </html>
-  );
+  )
 }
