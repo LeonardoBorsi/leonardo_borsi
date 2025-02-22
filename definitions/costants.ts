6336d20c0fdde6ed702cc5829e02b2e4e302ddd1
Leonardo Borsi <leonardo.borsi@gmail.com>
2025-02-22 16:18:56 +0100

    Landing page update

--- definitions/costants.ts
+++ definitions/costants.ts
@@ -1,8 +1,14 @@
 export const PAGE_ANCHORS = [
   { label: 'Work Experience', href: '#work-experience' },
+  { label: 'Personal Projects', href: '#personal-projects' },
   { label: 'Education', href: '#education' },
   { label: 'Certifications', href: '#certifications' }
 ]
+
+export const MEMOJI_SIZE = 190
+export const FULLNAME = 'Leonardo Borsi'
+export const MAIN_JOB_TITLE = 'Software Engineer'
+export const SECONDARY_JOB_TITLE = 'UX/UI Designer'
 
 export const GITHUB_PROFILE = 'https://github.com/LeonardoBorsi'
 export const LINKEDIN_PROFILE = 'https://www.linkedin.com/in/leonardo-borsi-b02403188/'
--- sections/landing-page.tsx
+++ sections/landing-page.tsx
@@ -1,88 +1,52 @@
 import { DownloadIcon, GithubIcon, LinkedinIcon, SendIcon } from '@/components/icons'
-import { GITHUB_PROFILE, LINKEDIN_PROFILE, PAGE_ANCHORS } from '@/definitions/costants'
+import { FULLNAME, GITHUB_PROFILE, LINKEDIN_PROFILE, MAIN_JOB_TITLE, MEMOJI_SIZE, SECONDARY_JOB_TITLE } from '@/definitions/costants'
 import { Button } from '@nextui-org/button'
 import Link from 'next/link'
 
 export default function LandingPage() {
-  const renderMemoji = () => (
-    <div>
-      <div className='block dark:hidden'>
-        {/* <video className='w-[250px] lg:w-[350px]' autoPlay={true} loop muted>
-          <source src='/videos/memoji-light.mp4' type='video/mp4' />
-        </video> */}
-        <img className='w-[250px] lg:w-[350px]' src='/gifs/memoji-light.gif'></img>
-      </div>
-      <div className='hidden dark:block'>
-        {/*  <video className='w-[250px] lg:w-[350px]' autoPlay={true} loop muted>
-          <source src='/videos/memoji-dark.mp4' type='video/mp4' />
-        </video> */}
-        {/* <img className='w-[250px] lg:w-[350px]' src='/videos/memoji-dark.gif'></img> */}
-      </div>
-    </div>
-  )
-
-  const renderTitles = () => (
-    <div>
-      <h1 className='hidden lg:block text-5xl lg:text-7xl font-medium'>Leonardo Borsi</h1>
-      <h2 className='text-4xl sm:text-5xl font-thin mt-3'>Software Engineer</h2>
-      <h3 className='text-3xl sm:text-4xl font-thin text-primary mt-3'>UX/UI Designer</h3>
-    </div>
-  )
-
-  const renderButtons = () => (
-    <div className='flex flex-col sm:flex-row justify-center lg:justify-start gap-5'>
-      <div className='flex gap-5'>
-        <Link className='w-1/2' target='_blank' href={GITHUB_PROFILE}>
-          <Button isIconOnly className='text-sm font-medium border-default-700 w-full' variant='bordered' size='lg'>
-            <GithubIcon size={28} />
-          </Button>
-        </Link>
-        <Link className='w-1/2' target='_blank' href={LINKEDIN_PROFILE}>
-          <Button isIconOnly className='text-sm font-medium border-default-700 w-full' variant='bordered' size='lg'>
-            <LinkedinIcon size={28} />
-          </Button>
-        </Link>
-      </div>
-      <Button
-        as={Link}
-        className='text-base font-medium border-default-700'
-        href='/files/Leonardo Borsi - CV.pdf'
-        startContent={<DownloadIcon />}
-        variant='bordered'
-        size='lg'
-        target='_blank'
-      >
-        Download CV
-      </Button>
-      <Button as={Link} color='primary' className='text-base font-medium bg-primary' href='#contacts' startContent={<SendIcon />} variant='solid' size='lg'>
-        Contact Me
-      </Button>
-    </div>
-  )
-
-  const renderAnchors = () => (
-    <div className='flex flex-col items-center lg:items-start gap-5'>
-      {PAGE_ANCHORS.map((link, i) => (
-        <Link key={i} className='text-[32px] font-light hover:text-primary w-fit' href={link.href}>
-          {link.label}
-        </Link>
-      ))}
-    </div>
-  )
-
   return (
-    <section>
-      <div className='h-screen pb-20 lg:pb-32'>
-        <div className='h-full flex flex-col lg:flex-row justify-between py-10 lg:py-20 gap-10'>
-          <div className='text-center lg:text-left flex flex-col justify-between space-y-28'>
-            <div className='space-y-10'>
-              {renderTitles()}
-              <div className='hidden lg:block'>{renderButtons()}</div>
-            </div>
-            <div className='hidden lg:block'>{renderAnchors()}</div>
+    <section className='h-screen pb-20 lg:pb-32 '>
+      <div className='h-full flex flex-col justify-between items-center gap-5'>
+        <div className='space-y-2 pt-5 lg:pt-0 text-center'>
+          <h1 className='hidden lg:block text-5xl lg:text-7xl font-medium'>{FULLNAME}</h1>
+          <h2 className='text-4xl sm:text-5xl font-thin mt-3'>{MAIN_JOB_TITLE}</h2>
+          <h3 className='text-3xl sm:text-4xl font-thin text-primary mt-3'>{SECONDARY_JOB_TITLE}</h3>
+        </div>
+        <div>
+          <div className='block dark:hidden'>
+            <img className='rounded-full' src='/images/memoji-light.jpeg' style={{ width: MEMOJI_SIZE, height: MEMOJI_SIZE }} />
           </div>
-          <div className='flex items-center justify-center lg:pr-10'>{/* renderMemoji() */}</div>
-          <div className='block lg:hidden'>{renderButtons()}</div>
+          <div className='hidden dark:block'>
+            <img className='rounded-full' src='/images/memoji-dark.jpeg' style={{ width: MEMOJI_SIZE, height: MEMOJI_SIZE }} />
+          </div>
+        </div>
+        <div className='w-full flex flex-col sm:flex-row justify-center gap-3'>
+          <div className='flex gap-3'>
+            <Link className='w-1/2' target='_blank' href={GITHUB_PROFILE}>
+              <Button isIconOnly className='text-sm font-medium border-default-700 w-full' variant='bordered' size='lg'>
+                <GithubIcon size={28} />
+              </Button>
+            </Link>
+            <Link className='w-1/2' target='_blank' href={LINKEDIN_PROFILE}>
+              <Button isIconOnly className='text-sm font-medium border-default-700 w-full' variant='bordered' size='lg'>
+                <LinkedinIcon size={28} />
+              </Button>
+            </Link>
+          </div>
+          <Button
+            as={Link}
+            className='text-base font-medium border-default-700'
+            href='/files/Leonardo Borsi - CV.pdf'
+            startContent={<DownloadIcon />}
+            variant='bordered'
+            size='lg'
+            target='_blank'
+          >
+            Download CV
+          </Button>
+          <Button as={Link} color='primary' className='text-base font-medium bg-primary' href='#contacts' startContent={<SendIcon />} variant='solid' size='lg'>
+            Contact Me
+          </Button>
         </div>
       </div>
     </section>
